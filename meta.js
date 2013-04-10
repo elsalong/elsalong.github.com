2dea51435a1d6e61f1645fadf883430219ebe3a5
elsalong <elsalonglove@gmail.com>
2013-04-11 00:06:28 +0800

    add js files

---
+++ meta.js
@@ -0,0 +1,12 @@
+{
+    "site_name": "ELSALONG",
+    "copyright": "Elsa",
+    "cates": [
+        {"name": "tech", "text":"技术"},
+        {"name": "food", "text":"美食"},
+        {"name": "other", "text":"其它"}
+    ],
+    "articles":[
+        {"title": "My first blog", "file": "2013-04-10", "cate":"other"}
+    ]
+}
